f7422a5f06ec3b4fcfce1f254f4b259f8dbb7ec8
celuu <christine.e.luu@gmail.com>
2022-12-15 14:32:07 -0800

    Finished set up

--- practice-for-week-15-react-redux-grocery-store-long-practice/src/index.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import configureStore from "./store";
+import "./index.css";
+import App from "./App";
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
 
 function Root() {
   return (
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </Provider>
   );
 }
 
@@ -16,5 +26,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
